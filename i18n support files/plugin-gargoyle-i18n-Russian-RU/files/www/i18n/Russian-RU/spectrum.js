dd0c35ca8a90fd0b70fffb2990c2b54aef9fd521
Michael Gray <michael.gray@lantisproject.com>
2016-02-20 14:42:59 +1100

    Handle non-HT networks, add russian translation

---
+++ i18n support files/plugin-gargoyle-i18n-Russian-RU/files/www/i18n/Russian-RU/spectrum.js
@@ -0,0 +1,7 @@
+﻿/*
+ * UTF-8 (with BOM) Polish-PL text strings for spectrum_analyser.sh html elements
+ */
+
+spectrum.Noscan="Сканирование не дало результатов!\nОбновите страницу для обновления, либо выберите другой диапазон.";
+spectrum.Analyser="Анализатор спектра";
+
